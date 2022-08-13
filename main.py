bc8e37e86c7e038b3768e6d690fbe025feb0b72d
Your Name <shubham10dulkar17@gmail.com>
2022-08-13 19:00:04 +0100

    UI changes and added seprator

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import secrets
 from tkinter import *
 #from tkinter.ttk import *
 import os
 import tkinter as tk
+from tkinter import ttk
 from tkinter import filedialog
 from tkinter.filedialog import FileDialog
 from turtle import fd
@@ -59,10 +61,12 @@ def createReactDockerFile(appName, portNo, dir):
 if __name__ == '__main__':
     screen1 = Tk()
     screen1.title("Project02")
-    screen1.geometry('1920x1920')
-    Label(screen1,text="Automated Dockerization",font=('Arial',15)).grid(column = 2,row=1)
+    screen1.geometry('1520x900')
+    screen1.resizable(False, False)
+    screen1.bind('<Escape>',lambda e: screen1.destroy())
+    Label(screen1,text="Automated Dockerization",font=('bold',20)).grid(column = 4,row=1)
 
-    Label(screen1,text="Select Directory",background='red',foreground='white',font=('Arial',15)).grid(column = 1, row = 2, padx=12, pady=25)
+    Label(screen1,text="Select Directory",background='red',foreground='white',font=('Arial',15)).grid(column = 3, row = 2, padx=12, pady=15)
 
 
     #--------------directory-----------------
@@ -75,24 +79,25 @@ if __name__ == '__main__':
 
     #--------------directory-----------------
 
-    #React.js
+    #----------------React.js----------------
 
-    Label(screen1, text="Enter Project Name : ",font=('Arial',15)).grid(column=2, row=5)
+    Label(screen1,text="ReactJS",background='blue',foreground='white',font=('Arial',20)).grid(column = 2, row = 3, padx=12, pady=10)
 
-    Label(screen1,text="ReactJS",background='blue',foreground='white',font=('Arial',15)).grid(column = 1, row = 3, padx=12, pady=25)
+    Label(screen1, text="Enter Project Name : ",font=('Arial',15)).grid(column=3, row=3)
+
 
     myReactProjectName = Entry(screen1,width=30,font=('Arial',15) )
-    myReactProjectName.insert(0, 'TodoApp')
-    myReactProjectName.grid(column=3, row=5, padx=10, pady=10,ipady=5)
+    myReactProjectName.insert(0, 'ReactJS Project Name')
+    myReactProjectName.grid(column=4, row=3, padx=10, pady=10,ipady=5)
 
 
-    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=2, row=6)
+    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=3, row=4)
 
     portNumberReact = Entry(screen1, width=27,font=('Arial',15))
     portNumberReact.insert(0, 'Ex. 8080')
-    portNumberReact.grid(column=3, row=6, padx=12, pady=10,ipady=5)
+    portNumberReact.grid(column=4, row=4, padx=12, pady=10,ipady=5)
 
-    button2 = Button(text="Browse",font=('Arial',15) , command=lambda : browse_button(screen1)).grid(row=2, column=2)
+    button2 = Button(text="Browse",font=('Arial',15) , command=lambda : browse_button(screen1)).grid(row=2, column=4)
 
    
     
@@ -102,104 +107,91 @@ if __name__ == '__main__':
             command=lambda : createReactDockerFile(myReactProjectName.get(), portNumberReact.get(), screen1.directory),
             fg="green",
             bg="white",
-            font=('Arial',15)).grid(column=3, row=7,ipady=5)
-    # # create volume for Wordpress
-    # Label(screen1, text="Enter Volume name for wordpress : ",font=('Arial',15)).grid(column=2, row=5)
+            font=('Arial',15)).grid(column=5, row=4,ipady=5)
 
-    # createVolWordpress = Entry(screen1, width=27,font=('Arial',15))
-    # createVolWordpress.insert(0, 'Enter Vol name for wordpress')
-    # createVolWordpress.grid(column=3, row=5, padx=10, pady=10,ipady=5)
+     #--------------------ReactJS------------------
+    separator = ttk.Separator(screen1, orient='horizontal')
+    separator.place(relx=0, rely=0.24, relwidth=1, relheight=1)
+ 
 
-    # # create volume for mysql
-    # Label(screen1,text="Enter Volume name for MYSQL : ",font=('Arial',15)).grid(column = 2,row=6)
+    #---------------------Wordpress----------------
+    Label(screen1,text="WordPress",background='orange',foreground='white',font=('Arial',20)).grid(column = 2, row = 6, padx=12, pady=10)
 
-    # createVol = Entry(screen1,width=27,font=('Arial',15))
-    # createVol.insert(0,'Enter Vol name for mysql')
-    # createVol.grid(column = 3,row=6,padx=10,pady=10,ipady=5)
+    # create volume for Wordpress
 
-    # Database Container name
-    # Label(screen1, text="Enter Database Container name (It will be your host name) : ",font=('Arial',15)).grid(column=2, row=7)
+    Label(screen1, text="Enter Volume name for wordpress : ",font=('Arial',15)).grid(column=3, row=6)
 
-    # mysqlContainerName = Entry(screen1, width=27,font=('Arial',15))
-    # mysqlContainerName.insert(0, 'Ex. dbos')
-    # mysqlContainerName.grid(column=3, row=7, padx=10, pady=25,ipady=5)
+    createVolWordpress = Entry(screen1, width=27,font=('Arial',15))
+    createVolWordpress.insert(0, 'Enter Vol name for wordpress')
+    createVolWordpress.grid(column=4, row=6, padx=10, pady=10,ipady=5)
+
+    # create volume for mysql
+    Label(screen1,text="Enter Volume name for MYSQL : ",font=('Arial',15)).grid(column = 3,row=7)
+
+    createVol = Entry(screen1,width=27,font=('Arial',15))
+    createVol.insert(0,'Enter Vol name for mysql')
+    createVol.grid(column = 4,row=7,padx=10,pady=10,ipady=5)
+
+    #Database Container name
+    Label(screen1, text="Enter Database Container name (It will be your host name) : ",font=('Arial',15)).grid(column=3, row=8)
+
+    mysqlContainerName = Entry(screen1, width=27,font=('Arial',15))
+    mysqlContainerName.insert(0, 'Ex. dbos')
+    mysqlContainerName.grid(column=4, row=8, padx=10, pady=10,ipady=5)
 
     # MySQL root password
-    Label(screen1, text="Mysql root password (MYSQL_ROOT_PASSWORD) : ",font=('Arial',15)).grid(column=2, row=8)
+    Label(screen1, text="Mysql root password (MYSQL_ROOT_PASSWORD) : ",font=('Arial',15)).grid(column=3, row=9)
 
     mysqlRootPassword = Entry(screen1, width=27,font=('Arial',15))
     mysqlRootPassword.insert(0, 'Ex. mydbos')
-    mysqlRootPassword.grid(column=3, row=8, padx=10, pady=10, ipady=5)
+    mysqlRootPassword.grid(column=4, row=9, padx=10, pady=10, ipady=5)
 
     # MySQL_USERNAME
-    Label(screen1, text="Enter username (MYSQL_USERNAME) : ",font=('Arial',15)).grid(column=2, row=9)
+    Label(screen1, text="Enter username (MYSQL_USERNAME) : ",font=('Arial',15)).grid(column=3, row=10)
 
     mySQLUsername = Entry(screen1, width=27,font=('Arial',15))
-    mySQLUsername.insert(0, 'Ex. ashu')
-    mySQLUsername.grid(column=3, row=9, padx=10, pady=10,ipady=5)
+    mySQLUsername.insert(0, 'Ex. username')
+    mySQLUsername.grid(column=4, row=10, padx=10, pady=10,ipady=5)
 
     # MySQL_PASSWORD
-    Label(screen1, text="Enter password (MYSQL_PASSWORD) : ",font=('Arial',15)).grid(column=2, row=10)
+    Label(screen1, text="Enter password (MYSQL_PASSWORD) : ",font=('Arial',15)).grid(column=3, row=11)
 
     mySQLPassword = Entry(screen1, width=27,font=('Arial',15))
-    mySQLPassword.insert(0, 'Ex. ashu@1999')
-    mySQLPassword.grid(column=3, row=10, padx=10, pady=10,ipady=5)
-
-    # # MySQL_HOST
-    # Label(screen1, text="Enter host (MYSQL_HOST) : ").grid(column=2, row=11)
-    #
-    # mySQLHost = Entry(screen1, width=27)
-    # mySQLHost.insert(0, 'Ex. localhost')
-    # mySQLHost.grid(column=3, row=11, padx=10, pady=25)
+    mySQLPassword.insert(0, 'Ex. password@1998')
+    mySQLPassword.grid(column=4, row=11, padx=10, pady=10,ipady=5)
 
     # MySQL_DATABASE_NAME
-    Label(screen1, text="Enter Database name (MYSQL_DATABASE_NAME) : ",font=('Arial',15)).grid(column=2, row=12)
+    Label(screen1, text="Enter Database name (MYSQL_DATABASE_NAME) : ",font=('Arial',15)).grid(column=3, row=12)
 
     mySQLDatabase = Entry(screen1,width=30,font=('Arial',15) )
     mySQLDatabase.insert(0, 'Ex. myDB')
-    mySQLDatabase.grid(column=3, row=12, padx=10, pady=10,ipady=5)
+    mySQLDatabase.grid(column=4, row=12, padx=10, pady=10,ipady=5)
 
     # Wordpress Container name
-    Label(screen1, text="Enter Wordpress Container Name : ",font=('Arial',15)).grid(column=2, row=13)
+    Label(screen1, text="Enter Wordpress Container Name : ",font=('Arial',15)).grid(column=3, row=13)
 
     wordpressContainerName = Entry(screen1, width=27,font=('Arial',15))
     wordpressContainerName.insert(0, 'Ex. myWebsite01')
-    wordpressContainerName.grid(column=3, row=13, padx=12, pady=10,ipady=5)
+    wordpressContainerName.grid(column=4, row=13, padx=12, pady=10,ipady=5)
 
     # Port Number
-    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=2, row=15)
+    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=3, row=14)
 
     portNumber = Entry(screen1, width=27,font=('Arial',15))
     portNumber.insert(0, 'Ex. 8080')
-    portNumber.grid(column=3, row=15, padx=12, pady=10,ipady=5)
+    portNumber.grid(column=4, row=14, padx=12, pady=10,ipady=5)
 
-    # Label(screen1,text=":",font=('Arial',15)).grid(column = 4, row = 15)
 
-    # portNumber2 = Entry(screen1, width=27,font=('Arial',15))
-    # portNumber2.insert(0, 'Ex. 80')
-    # portNumber2.grid(column=5, row=15, padx=12, pady=10,ipady=5)
-
-    Button(screen1, text="Submit", command=dockerManuall, fg="green", bg="white",font=('Arial',15)).grid(column=3, row=16,ipady=5)
+    Button(screen1, text="Submit", command=dockerManuall, fg="green", bg="white",font=('Arial',15)).grid(column=5, row=14,ipady=5)
 
     # One CLick auto configure
 
-    Label(screen1,text="One click and Auto Configure using docker compose file",background='green',foreground='white',font=('Arial',15)).grid(column = 3, row = 17, padx=12, pady=25)
+    #Label(screen1,text="One click and Auto Configure using docker compose file",background='green',foreground='white',font=('Arial',15)).grid(column = 3, row = 17, padx=12, pady=10)
 
-    btn1 = Button(screen1, text="Start", command=dockerCompose, fg="green", bg="white",font=('Arial',15))
-    btn1.grid(column=3, row=18,padx=15,pady=10,ipady=5)
+    btn1 = Button(screen1, text="Start WordPress", command=dockerCompose, fg="green", bg="white",font=('Arial',15))
+    btn1.grid(column=6, row=14,padx=15,pady=10,ipady=5)
 
 
-    # select language
-    def show():
-
-        label.config( text = clicked.get() )
-
-    # Dropdown menu options
-    options = [
-        "Express.js",
-        "React.js",
-        "Wordpress"
-    ]
 
     # datatype of menu text
     clicked = StringVar()
@@ -207,31 +199,31 @@ if __name__ == '__main__':
     # initial menu text
     clicked.set( "Select language" )
 
-    # Create Dropdown menu
-    # drop = OptionMenu( screen1 , clicked , *options )
-    # drop.grid(column=3, row=3)
 
-    # # Create button, it will change label text
-    # button = Button( screen1 , text = "Click Me" , command = show , font=('Arial',15)).grid(column=3, row = 4)
+   
+    #---------------------Wordpress----------------
 
-    # # Create Label
-    # label = Label( screen1 , text = " " )
-    # label.grid()
+    separator = ttk.Separator(screen1, orient='horizontal')
+    separator.place(relx=0, rely=0.82, relwidth=1, relheight=1)
 
-    #Express.js
+    #--------------Express.js--------------------
+    Label(screen1,text="ExpressJS",background='red',foreground='white',font=('Arial',20)).grid(column = 2, row = 15, padx=12, pady=10)
 
-    Label(screen1, text="Enter Project Name : ",font=('Arial',15)).grid(column=2, row=19)
+    Label(screen1, text="Enter Project Name : ",font=('Arial',15)).grid(column=3, row=15)
 
     myExpressProjectName = Entry(screen1,width=30,font=('Arial',15) )
     myExpressProjectName.insert(0, 'Ex. myDB')
-    myExpressProjectName.grid(column=3, row=19, padx=10, pady=10,ipady=5)
+    myExpressProjectName.grid(column=4, row=15, padx=10, pady=10,ipady=5)
 
 
-    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=2, row=20)
+    Label(screen1, text="Port Number : ",font=('Arial',15)).grid(column=3, row=16)
 
     portNumberEX = Entry(screen1, width=27,font=('Arial',15))
     portNumberEX.insert(0, 'Ex. 8080')
-    portNumberEX.grid(column=3, row=20, padx=12, pady=10,ipady=5)
+    portNumberEX.grid(column=4, row=16, padx=12, pady=10,ipady=5)
+
+
+    #--------------Express.js--------------------
 
 
 
